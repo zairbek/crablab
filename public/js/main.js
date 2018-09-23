5ec5bb01982feb608a969b22bba9eefbc852dbf1
zairbek <xaiker007@gmail.com>
2018-09-24 00:31:40 +0300

    24.09.18

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,4 @@
-window.onload = init;
+window.addEventListener("load", init)
 
 function init(){
   stretchBlock(getId("intro"));
@@ -7,31 +7,103 @@ function init(){
 }
 
 
-
+// FINISHED========
 getId = (attr) => document.getElementById(attr);
-(window.onresize = function(){
-  stretchBlock(getId("intro"));
-})
 
+window.onresize = function(){
+  stretchBlock(getId("intro"));
+};
 
 function stretchBlock(attr){
-  // let w = window.innerWidth;
   let h = window.innerHeight;
-
-  attr.style.height = (h - 20) + "px";
+  attr.style.height = h + "px";
 }
+// FINISHED========
+
+
+// ===================================================================
+(function(){
+  window.device = {};
+
+  var _user_agent = window.navigator.userAgent.toLowerCase();
+  
+  var device_desktop = ['windows', 'x11', 'macintosh'];
+  var device_mobile = ['android', 'blackberry', 'bb10', 'rim', 'ios', 'mobile', 'ipad', 'ipod', 'iphone', 'phone', 'touch'];
+
+  desktop = function(){
+    for(let i = 0; i <= device_desktop.length; i++){
+      if(_user_agent.indexOf(device_desktop[i]) !== -1)
+        return _user_agent.indexOf(device_desktop[i]) !== -1; 
+    }
+  }
+  mobile = function(){
+    for(let i = 0; i <= device_mobile.length; i++){
+      if(_user_agent.indexOf(device_mobile[i]) !== -1)
+        return _user_agent.indexOf(device_mobile[i]) !== -1
+    }
+  }
+
+
+  
+  // if(desktop())
+  //   alert("desktop")
+  // if(mobile())
+  //   alert("mobile")
+
+
+}).call(this);
+
+// =====================================================================================
+
+
 
 function scrollToElem(){
-  let scrollTo = this.dataset.scrollToElemId;
-  let to = getId(scrollTo).offsetTop;
-  // let scroll = window.pageYOffset;
- 
-  window.scrollBy(0, to);
+  var toEl = getId(this.dataset.scrollToElemId);
+  var offset =  toEl.offsetTop;
+  var nowPosition = window.pageYOffset;
+  var t;
+    
+  t = setInterval(function(){
+    if(nowPosition <= offset){
+    let p = window.pageYOffset; 
+    window.scrollTo(0, p + 8);
+
+      if(p >= offset){
+        clearInterval(t);          
+      }
+    }
+  }, 0.01)
+  
+
+
+  // window.scrollTo(0, offset);
 }
 
+
+
+
+
+// FINISHED========
 function swipeMenu(){
-  let el = this.dataset.elem;
-  getId(el).style.left = 0;
+  var el = getId(this.dataset.elem);
+  el.style.left = 0;
+
+  getId(el.dataset.elClose).addEventListener('click', function(){
+    el.style.left = - el.clientWidth - 5 + "px";
+  })
 }
+// FINISHED========
 
 
+
+
+
+
+
+
+
+
+
+function foo(dur){
+
+}
